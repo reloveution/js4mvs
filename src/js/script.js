d7c23cb86744504ade6bb23c767457dd5c0e2493
Michael Kolinski <michaelkolinski150@gmail.com>
2022-09-06 22:12:40 +0400

    variables, camelCase

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// 1 Переменные, camelCase
+const firstName = 'Mike'
+let lastName = 'Chukalenko'
+let age = 35
+const isProgrammer = true
+lastName = 'Kolinski'
+
+console.log(`FirstName: ${firstName}, Lastname: ${lastName}, Age: ${age}`)
+console.log(isProgrammer)
+
+const _ = 'private'
+const $ = 'some value'
+const withNum5 = '5'
+// const if = '' // err
+// const 5withNum = '' // err
