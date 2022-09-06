6f2618ba26201d5c88aec50590e3c18ed05eb781
Michael Kolinski <michaelkolinski150@gmail.com>
2022-09-06 22:37:03 +0400

    mutation

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,21 @@
-// 1 Переменные, camelCase
-const firstName = 'Mike'
-let lastName = 'Chukalenko'
-let age = 35
-const isProgrammer = true
-lastName = 'Kolinski'
+// // 1 Переменные, camelCase
+// const firstName = `Mike`
+// let lastName = `Chukalenko`
+// let age = 35
+// const isProgrammer = true
+// lastName = `Kolinski`
+// lastName = prompt('Input Lastname')
+// console.log(`FirstName: ${firstName}, Lastname: ${lastName}, Age: ${age}`)
+// console.log(isProgrammer)
 
-console.log(`FirstName: ${firstName}, Lastname: ${lastName}, Age: ${age}`)
-console.log(isProgrammer)
+// const _ = `private`
+// const $ = `some value`
+// const withNum5 = `5`
+// // const if = '' // err
+// // const 5withNum = '' // err
 
-const _ = 'private'
-const $ = 'some value'
-const withNum5 = '5'
-// const if = '' // err
-// const 5withNum = '' // err
+
+// // 2 Мутирование
+// // console.log(`Name: ` + firstName + `, Lastname: ${lastName}, Age: ${age}`)
+// alert(firstName + ' ' + lastName)
+
