d8ff43e2e879afe92f5fdb60521c816d7c9ceed2
Michael Kolinski <michaelkolinski150@gmail.com>
2022-09-06 22:55:08 +0400

    Operators

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+console.clear
 // // 1 Переменные, camelCase
 // const firstName = `Mike`
 // let lastName = `Chukalenko`
@@ -19,25 +20,34 @@
 // // console.log(`Name: ` + firstName + `, Lastname: ${lastName}, Age: ${age}`)
 // alert(firstName + ' ' + lastName)
 
-// 3 Operators
-let currentYear = 2022
-const birthYear = 1987
-let age = currentYear - birthYear
-console.log(age)
+// // 3 Operators
+// let currentYear = 2022
+// const birthYear = 1987
+// let age = currentYear - birthYear
+// console.log(age)
 
-const a = 10
-const b = 2
-let c = 32
+// const a = 10
+// const b = 2
+// let c = 32
 
-// c = c + a
-c += a
-c -= a
-c *= a
-c /= a
+// // c = c + a
+// c += a
+// c -= a
+// c *= a
+// c /= a
 
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(++currentYear)
-console.log(c)
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(++currentYear)
+// console.log(c)
+
+// 4 Data types
+const isProgrammer = true
+const name = `Mike`
+const age = 35
+
+console.log(typeof(isProgrammer))
+console.log(typeof(name))
+console.log(typeof(age))
