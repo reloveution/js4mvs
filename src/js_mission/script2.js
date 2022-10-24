84c13a1172fcc959f87b238dbe28a673fd58e63c
Michael Kolinski <michaelkolinski150@gmail.com>
2022-10-25 00:06:32 +0400

    4.2 finish

--- src/js_mission/script2.js
+++ src/js_mission/script2.js
@@ -1,65 +1,49 @@
+'use strict'
 console.clear()
-
-// let nString = ''
-
-// for (let i = 0; i < 20; i++){
-//     nString += Math.floor(Math.random() * 10);
-// }
-
-// console.log(nString)
-
-// for (let i = 0; i < nString.length; i++){
-//     let n = Number(nString[i])
-//     for (let ii = i+1; (n < 10) || (ii < nString.length); ii++){
-//         n += Number(nString[ii])
-//         if (n === 10) {
-//             console.log('----match----')
-//             console.log(`Start position is ${i+1}, end position is ${ii+1}`)
-//             let iii = 0
-//             while (iii <= (ii - i)) {
-//                 console.log(nString[i+iii]);
-//                 iii++;
-//             }
-//         }
-//     } 
-// }
-
-function makeString(l) {
-    let stroke = ''
-    for (let i = 0; i < l; i++){
-        stroke += Math.floor(Math.random() * 10);
-    }
-    return stroke
-}
-
 
 class Rstring {
     constructor(l) {
-        nString : makeString(l)
+        this.nString = this.makeString(l)
+    }
+
+    makeString(l) {
+        let stroke = ''
+        for (let i = 0; i < l; i++){
+            stroke += Math.floor(Math.random() * 10);
+        }
+        return stroke
     }
 
     match(x) {
         console.log(this.nString)
-
+        let iii = 0
         for (let i = 0; i < this.nString.length; i++){
+            let nn = this.nString[i]
             let n = Number(this.nString[i])
             for (let ii = i+1; (n < x) || (ii < this.nString.length); ii++){
+                nn += this.nString[ii]
                 n += Number(this.nString[ii])
                 if (n === x) {
-                    console.log('----match----')
+                    console.log(`----match for sum ${x}----`)
+                    console.log(nn)
                     console.log(`Start position is ${i+1}, end position is ${ii+1}`)
-                    let iii = 0
-                    while (iii <= (ii - i)) {
-                        console.log(this.nString[i+iii]);
-                        iii++;
-                    }
+                    iii++
+                    // while (iii <= (ii - i)) {
+                    //     console.log(this.nString[i+iii]);
+                    //     iii++;
+                    // }
                 }
             } 
         }
+        console.log('FFFFFFFFFFFFFFFFFFFFF')
+        console.log(`${iii} match in total`)
+        console.log('FFFFFFFFFFFFFFFFFFFFF')
     }
 
 }
 
-let string1 = new Rstring(10)
 
-string1.match(3)
+
+let string1 = new Rstring(55)
+
+string1.match(55)
